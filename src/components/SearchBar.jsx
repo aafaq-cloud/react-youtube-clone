a663fa887916d814605de93ece3724fe01817205
aafaq-cloud <Aafaqsikandar@gmail.com>
2023-03-13 13:35:25 +0500

    design navbar with material-ui components such as Paper, IconButton, Import icons from icons-material

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
+const SearchBar = () => {
+  // The paper is going to be a form
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        // Specific style for specific devices
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      {/* Button for icon */}
+      <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
